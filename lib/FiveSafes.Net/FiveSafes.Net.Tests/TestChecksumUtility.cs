1d0f01ece2552a6dea3ed02500266a27bbd3ac57
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-11-13 14:45:47 +0000

    AB#135535: FiveSafes.Net class lib creation (#4)
    * init FiveSafes.Net
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    * add test suite
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    * add ChecksumUtility and tests
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    ---------
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

---
+++ lib/FiveSafes.Net/FiveSafes.Net.Tests/TestChecksumUtility.cs
@@ -0,0 +1,25 @@
+using System.Text;
+using FiveSafes.Net.Utilities;
+
+namespace FiveSafes.Net.Tests;
+
+public class TestChecksumUtility
+{
+  [Fact]
+  public void ComputeSha512_Returns_CorrectChecksum()
+  {
+    // Arrange
+    const string input = "hello world";
+    const string expected =
+      "309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f";
+    var stream = new MemoryStream();
+    var writer = new BinaryWriter(stream, Encoding.UTF8);
+    writer.Write(input.ToArray());
+
+    // Act
+    var result = ChecksumUtility.ComputeSha512(stream);
+
+    // Assert
+    Assert.Equal(expected, result);
+  }
+}
