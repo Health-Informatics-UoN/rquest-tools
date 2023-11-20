b18802a9a4d293449ae89be7139516ba783f4059
vpnu <vasiliki.panagi@nottingham.ac.uk>
2023-11-20 16:51:56 +0000

    Add RQuestOptions.cs model

---
+++ app/RquestBridge/Models/RQuestOptions.cs
@@ -0,0 +1,11 @@
+namespace RquestBridge.Models;
+
+public class RQuestOptions
+{
+  public int Id { get; set; }
+
+  public string Host { get; set; } = string.Empty;
+  
+  public string ResourceId { get; set; } = string.Empty;
+  
+}
