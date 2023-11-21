4780fd164917f55e06ef940954ad43967a8e3493
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-11-21 11:35:52 +0000

    Save query file
    Co-authored-by: Vasiliki Panagi <vpnu@users.noreply.github.com>

---
+++ app/RquestBridge/Config/RquestQueryOptions.cs
@@ -0,0 +1,6 @@
+namespace RquestBridge.Config;
+
+public class RquestQueryOptions
+{
+  public const string FileName = "query.json";
+}
--- app/RquestBridge/Services/CrateGenerationService.cs
+++ app/RquestBridge/Services/CrateGenerationService.cs
@@ -1,6 +1,34 @@
+using System.Text.Json;
+using FiveSafes.Net;
+using RquestBridge.Dto;
+using RquestBridge.Config;
 namespace RquestBridge.Services;
 
 public class CrateGenerationService
 {
-  
+  public CrateGenerationService(){}
+
+  public async Task BuildCrate<T>(T job)
+  {
+    // TODO: build the initial archive
+
+    var payload = JsonSerializer.Serialize<T>(job);
+    var destination = Path.Combine("someDir", RquestQueryOptions.FileName);
+    await SaveJobPayload(payload, destination);
+  }
+
+  /// <summary>
+  /// Save a job payload to a file on disk.
+  /// </summary>
+  /// <param name="payload">The string to write to the file.</param>
+  /// <param name="destination">The path to the file to be created.</param>
+  /// <returns></returns>
+  private async Task SaveJobPayload(string payload, string destination)
+  {
+    var destinationInfo = new FileInfo(destination);
+
+    using var fileStream = destinationInfo.Create();
+    using var writer = new StreamWriter(fileStream);
+    await writer.WriteAsync(payload);
+  }
 }
