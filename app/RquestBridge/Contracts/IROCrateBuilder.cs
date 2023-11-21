010d2bf14de8fa7532afdbcc265c6fa54100d042
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-11-21 14:42:06 +0000

    crate builder interface

---
+++ app/RquestBridge/Contracts/IROCrateBuilder.cs
@@ -0,0 +1,12 @@
+using ROCrates.Models;
+
+namespace RquestBridge.Contracts;
+
+public interface IROCrateBuilder
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  RootDataset BuildRootDataset();
+}
