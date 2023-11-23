14e48563e017dbb1414a99edd20c6d17971f3908
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-11-23 15:32:45 +0000

    first test
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- tests/RquestBridge.Tests/TestRQuestWorkflowCrateBuilder.cs
+++ tests/RquestBridge.Tests/TestRQuestWorkflowCrateBuilder.cs
@@ -1,4 +1,6 @@
-using RquestBridge;
+using RquestBridge.Config;
+using RquestBridge.Utilities;
+
 namespace RquestBridge.Tests;
 
 public class TestRQuestWorkflowCrateBuilder
@@ -7,9 +9,22 @@ public class TestRQuestWorkflowCrateBuilder
   public void AddProfile_Adds_ProfileAsConfigured()
   {
     // Arrange
+    var workflowOptions = new WorkflowOptions();
+    var publishingOptions = new CratePublishingOptions();
+    var organisationOptions = new CrateOrganizationOptions();
+    var projectOptions = new CrateProjectOptions();
+    var agentOptions = new CrateAgentOptions();
+    var builder = new RQuestWorkflowCrateBuilder(workflowOptions, publishingOptions, agentOptions, projectOptions,
+      organisationOptions);
 
     // Act
+    builder.AddProfile();
+    var crate = builder.GetROCrate();
+    crate.Entities.TryGetValue("https://w3id.org/trusted-wfrun-crate/0.3", out var entity);
 
     // Assert
+    Assert.NotNull(entity);
+    Assert.Equal("https://w3id.org/trusted-wfrun-crate/0.3", entity.Id);
+    Assert.Equal("Profile", entity.GetProperty<string>("@type"));
   }
 }
