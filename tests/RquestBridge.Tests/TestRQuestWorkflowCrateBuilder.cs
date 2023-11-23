64c38cc12c36c7b463c702f30dea9f4fa2f042c8
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-11-23 15:03:19 +0000

    fix broken import

---
+++ tests/RquestBridge.Tests/TestRQuestWorkflowCrateBuilder.cs
@@ -0,0 +1,15 @@
+using RquestBridge;
+namespace RquestBridge.Tests;
+
+public class TestRQuestWorkflowCrateBuilder
+{
+  [Fact]
+  public void AddProfile_Adds_ProfileAsConfigured()
+  {
+    // Arrange
+
+    // Act
+
+    // Assert
+  }
+}
--- tests/RquestBridge.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace RquestBridge.Tests;
-
-public class UnitTest1
-{
-  [Fact]
-  public void Test1()
-  {
-  }
-}
