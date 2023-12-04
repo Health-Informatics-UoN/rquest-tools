9948ddd9de19fbae63268a7f9eeab02cc33d98f5
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-12-04 17:03:21 +0000

    configure options
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- app/RquestBridge/Program.cs
+++ app/RquestBridge/Program.cs
@@ -1,11 +1,28 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using RquestBridge.Config;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+#region Configure Services
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Options
+builder.Services
+  .Configure<RQuestOptions>(builder.Configuration.GetSection("RQuest"))
+  .Configure<RQuestTaskApiOptions>(builder.Configuration.GetSection("Credentials"))
+  .Configure<WorkflowOptions>(builder.Configuration.GetSection("Workflow"))
+  .Configure<CrateAgentOptions>(builder.Configuration.GetSection("Crate:Agent"))
+  .Configure<CrateProjectOptions>(builder.Configuration.GetSection("Crate:Project"))
+  .Configure<CrateOrganizationOptions>(builder.Configuration.GetSection("Crate:Organisation"))
+  .Configure<BridgeOptions>(builder.Configuration.GetSection("Bridge"));
+
+#endregion
+
 
 var app = builder.Build();
 
