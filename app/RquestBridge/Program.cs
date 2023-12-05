f299cd9cd4d0adbc1f08fda819e7767da48c9e57
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-12-05 15:00:52 +0000

    use startup pattern in Program.cs
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- app/RquestBridge/Program.cs
+++ app/RquestBridge/Program.cs
@@ -1,54 +1,12 @@
-﻿using RquestBridge.Config;
-using RquestBridge.Services;
-using RquestBridge.Services.Hosted;
+﻿using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using RquestBridge.Commands.Helpers;
+using RquestBridge.Startup;
+using RquestBridge.Startup.Cli;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-#region Configure Services
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Add Options
-builder.Services
-  .Configure<RQuestOptions>(builder.Configuration.GetSection("RQuest"))
-  .Configure<RQuestTaskApiOptions>(builder.Configuration.GetSection("Credentials"))
-  .Configure<WorkflowOptions>(builder.Configuration.GetSection("Workflow"))
-  .Configure<CrateAgentOptions>(builder.Configuration.GetSection("Crate:Agent"))
-  .Configure<CrateProjectOptions>(builder.Configuration.GetSection("Crate:Project"))
-  .Configure<CrateOrganizationOptions>(builder.Configuration.GetSection("Crate:Organisation"))
-  .Configure<BridgeOptions>(builder.Configuration.GetSection("Bridge"));
-
-// Add HttpClient
-builder.Services.AddHttpClient<RQuestTaskApiClient>();
-
-// Add Services
-builder.Services
-  .AddScoped<RQuestAvailabilityPollingService>()
-  .AddHostedService<RQuestPollingHostedService>()
-  .AddTransient<RabbitJobQueueService>()
-  .AddTransient<CrateGenerationService>();
-
-#endregion
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-  app.UseSwagger();
-  app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+await new CommandLineBuilder(new CliEntrypoint())
+  .UseDefaults()
+  .UseRootCommandBypass(args, WebEntrypoint.Run)
+  .UseCliHostDefaults(args)
+  .Build()
+  .InvokeAsync(args);
