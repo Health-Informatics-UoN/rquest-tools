c27cab8ecca7a7ea4079b0d52d3aca3ee439cbbf
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-12-06 17:12:32 +0000

    handle results method
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- app/RquestBridge/Services/ResultsHandlingService.cs
+++ app/RquestBridge/Services/ResultsHandlingService.cs
@@ -1,18 +1,48 @@
+using System.IO.Compression;
+using System.Text.Json;
 using RquestBridge.Config;
+using RquestBridge.Constants;
+using RquestBridge.Dto.RQuestResults;
 using RquestBridge.Models;
 
 namespace RquestBridge.Services;
 
-public class ResultsHandlingService
+public class ResultsHandlingService(RQuestTaskApiOptions taskApiOptions, MinioService minioService,
+  BridgeOptions bridgeOptions, RQuestTaskApiClient rQuestTaskApiClient, ILogger<ResultsHandlingService> logger)
 {
-  private readonly RQuestTaskApiOptions _taskApiOptions;
-
-  public ResultsHandlingService(RQuestTaskApiOptions taskApiOptions)
-  {
-    _taskApiOptions = taskApiOptions;
-  }
-
+  /// <summary>
+  /// Download the final results of a query from S3 and POST them back to RQuest.
+  /// </summary>
+  /// <param name="payload">
+  /// An object describing the final outcome of a request coming from a configured web hook.
+  /// </param>
   public async Task HandleResults(FinalOutcomeWebHookModel payload)
   {
+    var pathToResults = Path.Combine(bridgeOptions.WorkingDirectoryBase, payload.File);
+    var resultsDirectory =
+      Path.Combine(bridgeOptions.WorkingDirectoryBase, Path.GetFileNameWithoutExtension(pathToResults));
+    var resultsFile = Path.Combine(resultsDirectory, "data", RquestQuery.ResultFileName);
+
+    // Get file from S3
+    await minioService.GetFromStore(payload.File, pathToResults);
+
+    // Unzip results
+    ZipFile.ExtractToDirectory(pathToResults, resultsDirectory);
+
+    // Read results object
+    var resultsJson = await File.ReadAllTextAsync(resultsFile);
+
+    var results = JsonSerializer.Deserialize<RquestQueryResult>(resultsJson);
+    if (results is null)
+    {
+      logger.LogError("Could not deserialise results from {File}", resultsFile);
+      return;
+    }
+
+    // POST to RQuest
+    await rQuestTaskApiClient.ResultsEndpointPost(payload.SubId, results);
+
+    // Delete unpacked results
+    Directory.Delete(resultsDirectory, recursive: true);
   }
 }
