cf12768f5fa2204f2c276e113954ba136a8da411
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-12-07 10:22:40 +0000

    try catch for endpoint
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- app/RquestBridge/Controllers/JobsController.cs
+++ app/RquestBridge/Controllers/JobsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Minio.Exceptions;
 using RquestBridge.Models.WebHooks;
 using RquestBridge.Services;
 
@@ -9,12 +10,23 @@ namespace RquestBridge.Controllers;
 [AllowAnonymous]
 [Produces("application/json")]
 [Route("api/{controller}")]
-public class JobsController(ResultsHandlingService resultsHandlingService, ILogger logger) : ControllerBase
+public class JobsController(ResultsHandlingService resultsHandlingService) : ControllerBase
 {
   [HttpPost("complete")]
   public async Task<IActionResult> JobComplete(FinalOutcomeWebHookModel payload)
   {
-    await resultsHandlingService.HandleResults(payload);
-    return NoContent();
+    try
+    {
+      await resultsHandlingService.HandleResults(payload);
+      return NoContent();
+    }
+    catch (Exception e) when (e is MinioException)
+    {
+      /*
+       * `payload` contains invalid credentials for the configured MinIO instance
+       * or points to a non-existent bucket or object.
+       */
+      return BadRequest(e);
+    }
   }
 }
