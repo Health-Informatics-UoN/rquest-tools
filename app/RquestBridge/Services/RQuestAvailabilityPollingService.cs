09117c97860ba5ee3634f89d07a88ff2e16d5a3d
vpnu <vasiliki.panagi@nottingham.ac.uk>
2024-01-08 12:14:52 +0000

    Build BagIt in CrateGenerationService.cs

--- app/RquestBridge/Services/RQuestAvailabilityPollingService.cs
+++ app/RquestBridge/Services/RQuestAvailabilityPollingService.cs
@@ -7,34 +7,16 @@ using RquestBridge.Dto;
 
 namespace RquestBridge.Services;
 
-public class RQuestAvailabilityPollingService
+public class RQuestAvailabilityPollingService(RQuestTaskApiClient taskApi,
+  ILogger<RQuestAvailabilityPollingService> logger,
+  RabbitJobQueueService jobQueue,
+  CrateGenerationService crateGenerationService,
+  HutchApiClient hutchApiClient,
+  MinioService minioService,
+  IOptions<BridgeOptions> bridgeOptions)
 {
-  private readonly BridgeOptions _bridgeOptions;
-  private readonly CrateGenerationService _crateGenerationService;
-  private readonly HutchApiClient _hutchApiClient;
-  private readonly RabbitJobQueueService _jobQueue;
-  private readonly ILogger<RQuestAvailabilityPollingService> _logger;
-  private readonly MinioService _minioService;
-  private readonly RQuestTaskApiClient _taskApi;
+  private readonly BridgeOptions _bridgeOptions = bridgeOptions.Value;
 
-
-  public RQuestAvailabilityPollingService(
-    RQuestTaskApiClient taskApi,
-    ILogger<RQuestAvailabilityPollingService> logger,
-    RabbitJobQueueService jobQueue,
-    CrateGenerationService crateGenerationService,
-    HutchApiClient hutchApiClient,
-    MinioService minioService,
-    IOptions<BridgeOptions> bridgeOptions)
-  {
-    _logger = logger;
-    _taskApi = taskApi;
-    _jobQueue = jobQueue;
-    _crateGenerationService = crateGenerationService;
-    _hutchApiClient = hutchApiClient;
-    _minioService = minioService;
-    _bridgeOptions = bridgeOptions.Value;
-  }
 
   public async Task Poll(RQuestOptions rQuest)
   {
@@ -45,10 +27,10 @@ public class RQuestAvailabilityPollingService
       try
       {
         // Fetch RQuest Query
-        job = await _taskApi.FetchQuery<AvailabilityQuery>(rQuest);
+        job = await taskApi.FetchQuery<AvailabilityQuery>(rQuest);
         if (job is null)
         {
-          _logger.LogInformation(
+          logger.LogInformation(
             "No Queries on Collection: {CollectionId}",
             rQuest.CollectionId);
           return;
@@ -56,8 +38,7 @@ public class RQuestAvailabilityPollingService
 
         // Build RQuest RO-Crate
         var bagItPath = Path.Combine(_bridgeOptions.WorkingDirectoryBase, job.Uuid);
-        var archive = await _crateGenerationService.BuildBagIt(bagItPath);
-        await _crateGenerationService.BuildCrate(job, archive);
+        await crateGenerationService.BuildCrate(job, bagItPath);
 
         // Zip the BagIt package
         if (!Directory.Exists(bagItPath))
@@ -66,15 +47,15 @@ public class RQuestAvailabilityPollingService
         ZipFile.CreateFromDirectory(bagItPath, bagItPath + ".zip");
 
         // Write generated RO-Crate to store
-        await _minioService.WriteToStore(Path.Combine(_bridgeOptions.WorkingDirectoryBase, job.Uuid) + ".zip");
+        await minioService.WriteToStore(Path.Combine(_bridgeOptions.WorkingDirectoryBase, job.Uuid) + ".zip");
         // Submit RQuest Workflow RO-Crate to HutchAgent
-        await _hutchApiClient.HutchEndpointPost(job.Uuid);
+        await hutchApiClient.HutchEndpointPost(job.Uuid);
       }
       catch (Exception e)
       {
         if (job is null)
         {
-          _logger.LogError(e, "Task fetching failed");
+          logger.LogError(e, "Task fetching failed");
         }
         else
         {
@@ -97,7 +78,7 @@ public class RQuestAvailabilityPollingService
 
   private void SendToQueue(RQuestJob jobPayload, string queueName)
   {
-    _jobQueue.SendMessage(queueName, jobPayload);
-    _logger.LogInformation("Sent to Queue {Body}", JsonSerializer.Serialize(jobPayload));
+    jobQueue.SendMessage(queueName, jobPayload);
+    logger.LogInformation("Sent to Queue {Body}", JsonSerializer.Serialize(jobPayload));
   }
 }
