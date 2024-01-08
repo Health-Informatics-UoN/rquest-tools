be820c9919730647e5f1375aea0147b7f76b828d
vpnu <vasiliki.panagi@nottingham.ac.uk>
2024-01-08 12:10:16 +0000

    Add RootDataset and docstrings to constructors

--- lib/FiveSafes.Net/FiveSafes.Net/FiveSafesBagItBuilder.cs
+++ lib/FiveSafes.Net/FiveSafes.Net/FiveSafesBagItBuilder.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using FiveSafes.Net.Constants;
 using ROCrates;
 using ROCrates.Models;
@@ -10,14 +11,30 @@ public class FiveSafesBagItBuilder
   private readonly FiveSafesProfile _profile = new();
   private ROCrate _crate = new();
 
+  /// <summary>
+  /// Instantiates a blank BagIt archive in the current working directory.
+  /// </summary>
   public FiveSafesBagItBuilder()
   {
     _archive = new BagItArchive(Directory.GetCurrentDirectory());
   }
 
+  /// <summary>
+  /// Instantiates a blank BagIt archive in the given directory.
+  /// </summary>
+  /// <param name="archiveDirectory">The directory to instantiate it in.</param>
   public FiveSafesBagItBuilder(string archiveDirectory)
   {
     _archive = new BagItArchive(archiveDirectory);
+  }
+
+  /// <summary>
+  /// Load an existing BagItArchive. This is useful for modifying an existing archive.
+  /// </summary>
+  /// <param name="bagItArchive"></param>
+  public FiveSafesBagItBuilder(BagItArchive bagItArchive)
+  {
+    _archive = bagItArchive;
   }
 
   public async Task BuildChecksums()
@@ -40,9 +57,22 @@ public class FiveSafesBagItBuilder
 
   public void BuildCrate(string workflowUri)
   {
+    AddRootDataset();
     AddProfile();
     AddMainEntity(workflowUri);
     _crate.Save(_archive.PayloadDirectoryPath);
+  }
+
+  /// <summary>
+  /// Adds basic information for the metadata to ROCrate.
+  /// </summary>
+  private void AddRootDataset()
+  {
+    _crate.RootDataset.SetProperty("conformsTo", new Part
+    {
+      Id = _profile.Id,
+    });
+    _crate.RootDataset.SetProperty("datePublished", DateTimeOffset.UtcNow.ToString("o", CultureInfo.InvariantCulture));
   }
 
   private void AddProfile()
