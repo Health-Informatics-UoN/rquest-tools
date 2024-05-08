0c565d8e9bd52014c574b57fe198daa125bf4cdf
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-08 09:49:37 +0100

    remove unused deps
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- app/RquestBridge/Services/Hosted/RQuestPollingHostedService.cs
+++ app/RquestBridge/Services/Hosted/RQuestPollingHostedService.cs
@@ -1,6 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using RquestBridge.Config;
 
