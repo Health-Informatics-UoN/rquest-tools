cedaaf0049e505a3c2a078347345d0fde4ef3a61
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-31 17:08:56 +0100

    method for getting enum value

--- app/rquest-omop-worker/rquest_omop_worker/enums.py
+++ app/rquest-omop-worker/rquest_omop_worker/enums.py
@@ -7,3 +7,17 @@ class DistributionQueryType(str, Enum):
     DEMOGRAPHICS = "DEMOGRAPHICS"
     GENERIC = "GENERIC"
     ICD_MAIN = "ICD-MAIN"
+
+    @classmethod
+    def get_value(cls, value: str):
+        """Get the enum value of the distribution query type.
+
+        If no corressponding enum value exists, `None` will be returned.
+
+        Args:
+            value (str): The value to get the enum value for.
+
+        Returns:
+            Union[DistributionQueryType, None]: Return the enum value corresponding to `value` or `None`.
+        """
+        return cls._value2member_map_.get(value)
