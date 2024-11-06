8deb6dc971d74093dfb4a5f02293a2bfbd563a1d
James Mitchell-White <jamesjimitchell@gmail.com>
2024-11-06 15:55:25 +0000

    Added logging to daemon attempts

--- app/resolve/daemon/__init__.py
+++ app/resolve/daemon/__init__.py
@@ -2,13 +2,17 @@ import requests
 from os import environ
 import json
 import time
+import logging
 
+import core.config as config
 from core.execute_query import execute_query
 from core.parser import parser
 from core.rquest_dto.result import RquestResult
 
 
 def main() -> None:
+    args = parser.parse_args()
+    logger = logging.getLogger(config.LOGGER_NAME)
     result = execute_query(parser)
     if not isinstance(result, RquestResult):
         raise TypeError("Payload does not match RQuest result schema")
@@ -21,4 +25,5 @@ def main() -> None:
         if response.status_code == 200:
             break
         else:
+            logger.warning(f"Resolve failed to post to {url} at {time.time()}")
             time.sleep(5)
