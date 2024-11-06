bd44b42ea8841c975362eaeb7a29325e0ec58adb
James Mitchell-White <jamesjimitchell@gmail.com>
2024-11-06 15:17:07 +0000

    Resolve common function
    Both cli and daemon call execute query to get Rquest result
    
    CLI saves it
    Daemon POSTs it
    
    CLI still works, tested it

--- app/resolve/daemon/__init__.py
+++ app/resolve/daemon/__init__.py
@@ -1,2 +1,18 @@
+import requests
+from os import environ
+import json
+
+from core.execute_query import execute_query
+from core.parser import parser
+from core.rquest_dto.result import RquestResult
+
+
 def main() -> None:
-    print("hello rabbit world!")
+    result = execute_query(parser)
+    if not isinstance(result, RquestResult):
+        raise TypeError("Payload does not match RQuest result schema")
+    destination = environ.get("RESOLVE_DAEMON_POST_URL")
+    url = f"http://{destination}/{result.uuid}/{result.collection_id}"
+
+    data = json.dumps(result)
+    requests.post(url, data=data)
