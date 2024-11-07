48e705527c215ac84ad274a93017841152fd59bc
anwarfg <913028+anwarfg@users.noreply.github.com>
2024-11-07 14:07:10 +0000

    Added settings file and polling_temp. Settings has some testing code still in it. Polling is put in a separate file to keep it separate from the __init__. Should probably consider putting it in a class.

--- app/resolve/daemon/__init__.py
+++ app/resolve/daemon/__init__.py
@@ -1,2 +1,20 @@
+from settings import *
+import requests
+import polling
+
 def main() -> None:
     print("hello rabbit world!")
+    print ('testing')
+
+    retval = polling.poll(
+        lambda: requests.get(relay_base_url).status_code == 200,
+        step=polling_interval,
+        poll_forever=True)
+
+    print (retval)
+
+    pass
+pass
+
+if __name__ == '__main__':
+    main()
