1162f6b46e53be1d8a89ca7e8977b346ea050f05
Andy Rae <hello@rae.li>
2024-11-08 15:27:36 +0000

    Add test update result

--- tests/Hutch.Relay.Tests/Services/RelaySubTaskServiceTests.cs
+++ tests/Hutch.Relay.Tests/Services/RelaySubTaskServiceTests.cs
@@ -2,6 +2,7 @@ using Hutch.Relay.Data;
 using Hutch.Relay.Data.Entities;
 using Hutch.Relay.Models;
 using Hutch.Relay.Services;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 using Assert = Xunit.Assert;
 
@@ -17,7 +18,8 @@ public class RelaySubTaskServiceTests(Fixtures fixtures) : IClassFixture<Fixture
   public async Task DisposeAsync()
   {
     // Clean up the database after each test
-    _dbContext.RelayTasks.RemoveRange(_dbContext.RelayTasks);
+    _dbContext.RelaySubTasks.RemoveRange(_dbContext.RelaySubTasks);
+    _dbContext.RelayUsers.RemoveRange(_dbContext.RelayUsers);
     await _dbContext.SaveChangesAsync();
   }
   
@@ -55,5 +57,46 @@ public class RelaySubTaskServiceTests(Fixtures fixtures) : IClassFixture<Fixture
 
     var entityInDb = await _dbContext.RelaySubTasks.FindAsync(result.Id);
     Assert.NotNull(entityInDb);
+  }
+  
+  [Fact]
+  public async Task SetResult_ValidId_UpdatesResultAndReturnsRelaySubTaskModel()
+  {
+    // Arrange
+    var relaySubTask = new RelaySubTask
+    {
+      Id = "test-subtask-id",
+      RelayTask = new RelayTask { Id = "test-task-id-1" },
+      Owner = new SubNode
+      {
+        Id = "test-owner-id-1",
+        RelayUsers = new List<RelayUser>
+        {
+          new() { Id = "test-user-id-1", UserName = "testuser1@example.com" }
+        }
+      },
+      Result = null
+    };
+
+    _dbContext.RelaySubTasks.Add(relaySubTask);
+    await _dbContext.SaveChangesAsync();
+
+    var service = new RelaySubTaskService(_dbContext);
+
+    // Act
+    const string updatedResult = "Test Result";
+    var result = await service.SetResult(relaySubTask.Id, updatedResult);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Equal(relaySubTask.Id, result.Id);
+    Assert.Equal(updatedResult, result.Result); 
+    
+    var updatedSubTask = await _dbContext.RelaySubTasks
+      .Include(st => st.Owner)
+      .FirstOrDefaultAsync(st => st.Id == relaySubTask.Id);
+
+    Assert.NotNull(updatedSubTask);
+    Assert.Equal(updatedResult, updatedSubTask.Result);
   }
 }
