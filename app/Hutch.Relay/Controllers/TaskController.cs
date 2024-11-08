078f26d72a8885cb31abc19d08c87a2a822859b8
Jon Couldridge <jcouldridge@outlook.com>
2024-11-08 11:28:32 +0000

    nextjob dummy endpoint works

--- app/Hutch.Relay/Controllers/TaskController.cs
+++ app/Hutch.Relay/Controllers/TaskController.cs
@@ -28,18 +28,16 @@ public class TaskController : ControllerBase
   [SwaggerResponse(403)]
   [SwaggerResponse(404)]
   [SwaggerResponse(409)]
-  public async Task<IActionResult> Result(string uuid, string collectionId, [FromBody] JobResult result)
+  public Task<IActionResult> Result(string uuid, string collectionId, [FromBody] JobResult result)
   {
     throw new NotImplementedException();
   }
 
   # region Dummy Endpoints
 
-  private static readonly Random _random = new();
-
-  private static AvailabilityJob _dummyAvailability = new()
+  private static AvailabilityJob DummyAvailability(string collectionId) => new()
   {
-    Uuid = Guid.NewGuid().ToString(),
+    Uuid = "dummy-availability-task",
     Cohort = new()
     {
       Combinator = "OR",
@@ -63,30 +61,18 @@ public class TaskController : ControllerBase
     },
     Owner = "user1",
     CharSalt = "52ee5332-d209-4cf6-86d2-7f7569292c23",
-    ProtocolVersion = "v2"
+    ProtocolVersion = "v2",
+    Collection = collectionId
   };
 
-  private const CollectionAnalysisJob _dummyGeneric =
-    // """
-    // {
-    //   "code": "GENERIC",
-    //   "analysis": "DISTRIBUTION",
-    //   "uuid": "b152db31-e3ef-4ec7-9bed-c938cc2c0ac2",
-    //   "collection": "RQ-CC-d8e8c4aa-a8af-4229-b479-618788c44122",
-    //   "owner": "user1"
-    // }
-    // """;
-
-  private const CollectionAnalysisJob _dummyDemographics =
-    // """
-    // {
-    //   "code": "DEMOGRAPHICS",
-    //   "analysis": "DISTRIBUTION",
-    //   "uuid": "d16d3db6-0b07-4cea-be66-f1b54763e71c",
-    //   "collection": "RQ-CC-d8e8c4aa-a8af-4229-b479-618788c44122",
-    //   "owner": "user1"
-    // }
-    // """;
+  private static CollectionAnalysisJob DummyDistribution(string code, string collectionId) => new()
+  {
+    Code = code,
+    Analysis = "DISTRIBUTION",
+    Owner = "user1",
+    Uuid = "dummy-distribution-task",
+    Collection = collectionId,
+  };
 
   /// <summary>
   /// A dummy endpoint used for rapid testing.
@@ -96,26 +82,52 @@ public class TaskController : ControllerBase
   /// Unsupported query types always return 204.
   /// </summary>
   /// <returns></returns>
-  private Task<IActionResult> DummyNext(string collectionId)
+  private Task<IActionResult> DummyNext(string collectionIdParam)
   {
     // flip a coin and return nothing if false
     if (!Flip()) return Task.FromResult<IActionResult>(NoContent());
 
     // Otherwise, return based on the requested type
-    var queryType = collectionId.Split(".").Last();
+    var parts = collectionIdParam.Split(".");
+    var collectionId = parts[0];
+    var queryType = parts.Length > 1 ? parts[1] : null;
 
-    var payload = queryType switch
+    TaskApiBaseResponse? payload = queryType switch
     {
-      "a" => _dummyAvailability,
-      "b" => Flip() ? _dummyGeneric : _dummyDemographics, // random chance which type of distribution query
-      _ => queryType == collectionId ? _dummyAvailability : null
+      "a" => DummyAvailability(collectionId),
+      "b" => DummyDistribution(Flip() ? "GENERIC" : "DEMOGRAPHICS",
+        collectionId), // random chance which type of distribution query
+      _ => queryType is null ? DummyAvailability(collectionId) : null
     };
 
     return payload is null
       ? Task.FromResult<IActionResult>(NoContent())
       : Task.FromResult<IActionResult>(Ok(payload));
 
-    bool Flip() => _random.Next(0, 1) == 1;
+    bool Flip()
+    {
+      var rand = RandomGen2.Next(2);
+      return rand == 1;
+    }
+  }
+
+  private static class RandomGen2
+  {
+    private static readonly Random _global = new Random();
+    [ThreadStatic] private static Random? _local;
+
+    public static int Next(int max)
+    {
+      var inst = _local;
+      if (inst is null)
+      {
+        int seed;
+        lock (_global) seed = _global.Next();
+        _local = inst = new(seed);
+      }
+
+      return inst.Next(max);
+    }
   }
 
   #endregion
