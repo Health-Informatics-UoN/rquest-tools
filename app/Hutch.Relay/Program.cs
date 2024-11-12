1d37c8065c6ca730fbee8655e3f17abfb57131ef
vpnu <vasiliki.panagi@nottingham.ac.uk>
2024-11-12 15:06:39 +0000

    Add EfCoreMigrationsEntrypoint.cs

--- app/Hutch.Relay/Program.cs
+++ app/Hutch.Relay/Program.cs
@@ -3,6 +3,10 @@ using System.CommandLine.Builder;
 using System.CommandLine.Parsing;
 using Hutch.Relay.Commands.Helpers;
 using Hutch.Relay.Startup.Cli;
+using Hutch.Relay.Startup.EfCoreMigrations;
+
+// Enable EF Core tooling to get a DbContext configuration
+EfCoreMigrations.BootstrapDbContext(args);
 
 await new CommandLineBuilder(new CliEntrypoint())
   .UseDefaults()
