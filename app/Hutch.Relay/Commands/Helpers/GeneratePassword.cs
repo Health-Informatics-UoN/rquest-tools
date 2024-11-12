115662adb1bbffbb84653e2f71ec0f390134e393
vpnu <vasiliki.panagi@nottingham.ac.uk>
2024-11-12 13:25:18 +0000

    addUser command helper classes

---
+++ app/Hutch.Relay/Commands/Helpers/GeneratePassword.cs
@@ -0,0 +1,32 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Hutch.Relay.Commands.Helpers;
+
+public static class GeneratePassword
+{
+  
+  private static readonly char[] chars = ("abcdefghijklmnopqrstuvwxyz" +
+                                          "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                                          "0123456789" +
+                                          "$#_!%*&").ToCharArray();
+  public static string GenerateUniquePassword(int size)
+  {
+    byte[] data = new byte[4 * size];
+    using (var crypto = RandomNumberGenerator.Create())
+    {
+      crypto.GetBytes(data);
+    }
+
+    StringBuilder result = new StringBuilder(size);
+    for (int i = 0; i < size; i++)
+    {
+      var rnd = BitConverter.ToUInt32(data, i * 4);
+      var index = rnd % chars.Length;
+
+      result.Append(chars[index]);
+    }
+    Console.WriteLine(result);
+    return result.ToString();
+  }
+}
