4ec7a691f20e56d7da74ed8f56ffd06041c896f3
vpnu <vasiliki.panagi@nottingham.ac.uk>
2024-11-12 13:21:32 +0000

    remove unused import

--- app/Hutch.Relay/Models/RelayUserModel.cs
+++ app/Hutch.Relay/Models/RelayUserModel.cs
@@ -1,11 +1,9 @@
 using System.Collections.ObjectModel;
-using Hutch.Relay.Models;
 using Microsoft.AspNetCore.Identity;
 
 namespace Hutch.Relay.Models;
 
-public class RelayUserModel: IdentityUser
+public class RelayUserModel : IdentityUser
 {
   public ICollection<SubNodeModel> SubNodes { get; set; } = new Collection<SubNodeModel>();
-
 }
