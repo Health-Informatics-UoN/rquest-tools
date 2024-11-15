d4a5007888c64e4c84c50b7a1af7d553d9360288
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2024-11-15 12:09:33 +0000

    Results Endpoint (#133)
    * RackitApiClientException returns response when exception is thrown
    
    * Add Get() and ListIncomplete methods in RelaySubTaskService.cs
    
    * Create TaskApiService.cs, for submitting the results using Rackit
    
    * fix packages
    
    * Check for incomplete subtasks, submit if all complete
    
    * add new methods to IRelaySubTaskService.cs
    
    * add break if completed successfully
    
    * set task as complete after submitting
    
    * add TaskApiService.cs to ConfigureWebService.cs
    
    * change logging
    
    * remove comment
    
    * match results endpoint return to rquest
    
    * Await SubmitResults log success and break
    
    * add todo comment

--- lib/Hutch.Rackit/RackitApiClientException.cs
+++ lib/Hutch.Rackit/RackitApiClientException.cs
@@ -1,5 +1,6 @@
 namespace Hutch.Rackit;
 
-public class RackitApiClientException(string? message) : Exception(message)
+public class RackitApiClientException(string? message, HttpResponseMessage? response = null) : Exception(message)
 {
+  public HttpResponseMessage? UpstreamApiResponse { get; set; } = response;
 }
