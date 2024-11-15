2f83b7586501b86b86c00d92073a1e29192cf491
andrewthien <anretrithien@gmail.com>
2024-11-15 13:22:31 +0000

    Register Obfuscation service

---
+++ app/Hutch.Relay/Config/ObfuscationOptions.cs
@@ -0,0 +1,7 @@
+namespace Hutch.Relay.Config;
+
+public class ObfuscationOptions
+{
+  public int LowNumberSuppressionNumber { get; set; }
+  public int RoundingTarget { get; set; }
+}
--- app/Hutch.Relay/Startup/Web/ConfigureWebService.cs
+++ app/Hutch.Relay/Startup/Web/ConfigureWebService.cs
@@ -42,7 +42,11 @@ public static class ConfigureWebService
     builder.Services
       .AddTransient<IRelayTaskService, RelayTaskService>()
       .AddTransient<IRelaySubTaskService, RelaySubTaskService>()
-      .AddTransient<ISubNodeService, SubNodeService>()
+      .AddTransient<ISubNodeService, SubNodeService>();
+    
+    // Obfuscation
+    builder.Services
+      .Configure<ObfuscationOptions>(builder.Configuration.GetSection(("Obfuscation")))
       .AddTransient<IObfuscationService, ObfuscationService>();
 
     // Hosted Services
