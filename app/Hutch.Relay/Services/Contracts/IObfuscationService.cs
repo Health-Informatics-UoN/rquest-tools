4e49afd1dd7f8ca673161fe1609c481fc58bff50
andrewthien <anretrithien@gmail.com>
2024-11-18 11:47:03 +0000

    write the docs for new obfuscation functions

--- app/Hutch.Relay/Services/Contracts/IObfuscationService.cs
+++ app/Hutch.Relay/Services/Contracts/IObfuscationService.cs
@@ -1,22 +1,21 @@
-using Hutch.Relay.Models;
-
 namespace Hutch.Relay.Services.Contracts;
 
 public interface IObfuscationService
 {
 
-  // /// <summary>
-  // /// Get a RelayTask by id
-  // /// </summary>
-  // /// <param name="count">id of the RelayTask to get</param>
-  // /// <returns>The RelayTaskModel of the id</returns>
-  // /// <exception cref="KeyNotFoundException">The RelayTask does not exist.</exception>
+  /// <summary>
+  /// Suppresses numbers that are below a specified threshold.
+  /// </summary>
+  /// <param name="value">The value to be checked.</param>
+  /// <param name="threshold">The threshold below which the value will be suppressed.</param>
+  /// <returns>The original value if it is above the threshold, otherwise 0.</returns>
   int LowNumberSuppression(int value, int threshold);
 
-  // /// <summary>
-  // /// Create a new RelayTask
-  // /// </summary>
-  // /// <param name="model">Model to Create.</param>
-  // /// <returns>The newly created RelayTask.</returns>
+  /// <summary>
+  /// Rounds a value to the nearest specified number.
+  /// </summary>
+  /// <param name="value">The value to be rounded.</param>
+  /// <param name="nearest">The number to which the value will be rounded.</param>
+  /// <returns>The value rounded to the nearest specified number.</returns>
   int Rounding(int value, int nearest);
 }
