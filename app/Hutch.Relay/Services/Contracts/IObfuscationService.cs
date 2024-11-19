96c74fac5055d765807323c20c3d477d515b2fea
andrewthien <anretrithien@gmail.com>
2024-11-19 11:06:41 +0000

    add one more interface

--- app/Hutch.Relay/Services/Contracts/IObfuscationService.cs
+++ app/Hutch.Relay/Services/Contracts/IObfuscationService.cs
@@ -6,16 +6,22 @@ public interface IObfuscationService
   /// <summary>
   /// Suppresses numbers that are below a specified threshold.
   /// </summary>
-  /// <param name="value">The value to be checked.</param>
-  /// <param name="threshold">The threshold below which the value will be suppressed.</param>
+  /// <param name="value">The value to be checked and suppressed.</param>
   /// <returns>The original value if it is above the threshold, otherwise 0.</returns>
-  int LowNumberSuppression(int value, int threshold);
+  int LowNumberSuppression(int value);
 
   /// <summary>
   /// Rounds a value to the nearest specified number.
   /// </summary>
   /// <param name="value">The value to be rounded.</param>
-  /// <param name="nearest">The number to which the value will be rounded.</param>
   /// <returns>The value rounded to the nearest specified number.</returns>
-  int Rounding(int value, int nearest);
+  int Rounding(int value);
+
+
+  /// <summary>
+  /// Applies obfuscation functions to the value based on the obfuscation options.
+  /// </summary>
+  /// <param name="value">The value to be obfuscated.</param>
+  /// <returns>The obfuscated value.</returns>
+  int Obfuscate(int value);
 }
